fffd4d08fc76daf365f9840098f5c02a6bc43044
Sourav Sharma <souravsharma1003@gmail.com>
2026-06-09 10:36:01 +0530

    feat:add user model with password hashing

---
+++ server/models/User.js
@@ -0,0 +1,55 @@
+const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
+
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
+        lowercase: true
+    },
+    password: {
+        type: String,
+        required: true,
+        minLength: 6
+    },
+    role: {
+        type: String,
+        enum: ["user", "admin"],
+        default: "user"
+    },
+    avatar: {
+        type: String,
+        default: null
+    },
+    isVerified: {
+        type: Boolean,
+        default: false
+    },
+    refreshToken: {
+        type: String,
+        default: null
+    }
+}, { timestamps: true })
+
+
+userSchema.pre("save", async function (next) {
+    if (!this.isModified("password")) return next();
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+    next();
+})
+
+userSchema.methods.comparePassword = async function (password) {
+    const isPasswordSame = await bcrypt.compare(password, this.password);
+    return isPasswordSame;
+}
+
+const userModel = mongoose.model("User", userSchema);
+module.exports = userModel;
