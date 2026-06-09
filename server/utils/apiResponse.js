0aefbec0e302c5f090e0d71a6e9bfabf2d7e293f
Sourav Sharma <souravsharma1003@gmail.com>
2026-06-09 09:20:21 +0530

    feat: add centralized API response utility

---
+++ server/utils/apiResponse.js
@@ -0,0 +1,14 @@
+module.exports.successResponse=(res, message, data, statusCode=200)=>{
+    return res.status(statusCode).json({
+        success:true,
+        message,
+        data
+    })
+}
+
+module.exports.errorResponse=(res, message, statusCode=400)=>{
+    return res.status(statusCode).json({
+        success:false,
+        message
+    })
+}
